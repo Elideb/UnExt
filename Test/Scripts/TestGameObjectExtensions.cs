42c330ec9336cc90fcda85a85a25035ce0033b36
Elideb <elorahranma@gmail.com>
2013-12-27 04:41:40 +0100

    Added test/example classes.

---
+++ Test/Scripts/TestGameObjectExtensions.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnExt;
+
+public class TestGameObjectExtensions : MonoBehaviour {
+
+    private static bool firstTime = true;
+
+    public Transform transformPrefab;
+    public GameObject gameobjectPrefab;
+    public TestGameObjectExtensions scriptPrefab;
+    public SpriteRenderer componentPrefab;
+
+    void Start() {
+        if (TestGameObjectExtensions.firstTime) {
+            TestGameObjectExtensions.firstTime = false;
+
+            var trans = this.Instantiate<Transform>( transformPrefab );
+            trans.name = "Instantiate<Transform> " + trans.GetType();
+            trans = this.Instantiate<Transform>( transformPrefab, new Vector3( 2, 3, 2 ), Quaternion.identity );
+            trans.name = "Instatiate<Transform> " + trans.GetType() + " (translated)";
+
+            var go = this.Instantiate<GameObject>( gameobjectPrefab );
+            go.name = "Instantiate<GameObject> " + go.GetType();
+            go = this.Instantiate<GameObject>( gameobjectPrefab, new Vector3( 2, 5, 4 ), Quaternion.identity );
+            go.name = "Instantiate<GameObject> " + go.GetType() + " (translated)";
+
+            var sc = this.Instantiate<TestGameObjectExtensions>( scriptPrefab );
+            sc.name = "Instantiate<TestGameObjectExtensions> " + sc.GetType();
+
+            var sp = this.Instantiate<SpriteRenderer>( componentPrefab );
+            sp.name = "Instantiate<SpriteRenderer> " + sp.GetType();
+
+            var co = this.Instantiate<Component>( componentPrefab );
+            co.name = "Instantiate<Component> " + co.GetType();
+        }
+    }
+
+}
---
+++ Test/Scripts/TestTextureExtensions.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnExt;
+
+public class TestTextureExtensions : MonoBehaviour {
+
+    public Sprite sprite;
+    private Dictionary<int, Color> colors = new Dictionary<int, Color>() {
+        { 0, Color.black },
+        { 1, Color.blue },
+        { 2, Color.cyan },
+        { 3, Color.gray },
+        { 4, Color.green },
+        { 5, Color.grey },
+        { 6, Color.magenta },
+        { 7, Color.red },
+        { 8, Color.white },
+        { 9, Color.yellow }
+    };
+
+    public Sprite alphaSprite;
+
+    // Use this for initialization
+    void Start() {
+        var go = new GameObject( "Original cube", typeof( SpriteRenderer ) );
+        var spRenderer = go.GetComponent<SpriteRenderer>();
+        spRenderer.sprite = this.sprite;
+
+        go = new GameObject( "Alpha cube", typeof( SpriteRenderer ) );
+        go.transform.position = new Vector3( 0, 2, 0 );
+        spRenderer = go.GetComponent<SpriteRenderer>();
+        spRenderer.sprite = this.alphaSprite;
+    }
+
+    int alphaPainted = 1;
+
+    void OnGUI() {
+
+        if (GUI.Button( new Rect( 10, 10, 100, 15 ), "Outlined" )) {
+            for (int i = 0; i < 20; ++i) {
+                var go = new GameObject( string.Format( "cube {0:00}", i ) );
+                go.transform.position = new Vector3( 2 * i, 0, 0 );
+                var spRenderen = go.AddComponent<SpriteRenderer>();
+
+                var sourceTex = this.sprite.texture;
+                int colorIdx = i % this.colors.Count;
+                Color color = this.colors[colorIdx];
+                var newSprite = Sprite.Create( sourceTex.Outlined( color, (uint)i ),
+                                               new Rect( 0, 0, sourceTex.width, sourceTex.height ),
+                                               new Vector2( .5f, .5f ) );
+                newSprite.name = string.Format( "{0} {1:00}", this.sprite.name, i );
+                spRenderen.sprite = newSprite;
+            }
+        }
+
+        if (GUI.Button( new Rect( 10, 30, 100, 15 ), "Alpha" )) {
+            var sourceTex = this.alphaSprite.texture;
+            var newTexture = sourceTex.CreateCopy();
+            float cutoff = Random.Range( .05f, .8f );
+            newTexture.ClearAlpha( cutoff );
+            var newSprite = Sprite.Create( newTexture,
+                                           new Rect( 0, 0, sourceTex.width, sourceTex.height ),
+                                           new Vector2( .5f, .5f ) );
+            newSprite.name = string.Format( "{0} cutoff {1:.00}", sourceTex.name, cutoff );
+
+            var go = new GameObject( string.Format( "{0} cutoff {1:.00}", sourceTex.name, cutoff ) );
+            go.transform.position = new Vector3( this.alphaPainted * 2, 2, 0 );
+            var spRenderer = go.AddComponent<SpriteRenderer>();
+            spRenderer.sprite = newSprite;
+            ++this.alphaPainted;
+        }
+    }
+}
