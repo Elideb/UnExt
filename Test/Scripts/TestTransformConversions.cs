44fba288f35bfbeda0657fbfb61ef555852dc40f
Elideb <elorahranma@gmail.com>
2014-05-23 04:36:40 +0200

    Added test scenes for and scripts for Transform and Vector3 extension methods.

---
+++ Test/Scripts/TestTransformConversions.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnExt;
+
+namespace UnExt.Test {
+    public class TestTransformConversions : MonoBehaviour {
+
+        public Transform refPoint;
+        public Vector3 relPoint = new Vector3(4, 5, 2);
+
+        public Color xAxisColour = Color.red;
+        public Color yAxisColour = Color.green;
+        public Color zAxisColour = Color.blue;
+
+        public Color worldSpaceColour = Color.green;
+        public Color localSpaceColour = Color.blue;
+
+        void OnDrawGizmos() {
+            if (refPoint == null || relPoint == null) {
+                return;
+            }
+
+            var origColor = Gizmos.color;
+
+            DrawAxisGizmos(Vector3.zero, Quaternion.identity);
+            DrawAxisGizmos(refPoint.position, refPoint.rotation);
+
+            Gizmos.color = worldSpaceColour;
+            Gizmos.DrawWireSphere(refPoint.position, .5f);
+            Gizmos.DrawWireSphere(relPoint, .5f);
+
+
+            Gizmos.color = localSpaceColour;
+            Vector3 refLocalPoint = refPoint.PointToLocalSpace(refPoint.position);
+            Vector3 relLocalPoint = refPoint.PointToLocalSpace(relPoint);
+            Gizmos.DrawWireSphere(refLocalPoint, .25f);
+            Gizmos.DrawWireSphere(relLocalPoint, .25f);
+
+            Gizmos.color = worldSpaceColour;
+            Vector3 refWorlPoint = refPoint.PointToWorldSpace(refLocalPoint);
+            Vector3 relWorldPoint = refPoint.PointToWorldSpace(relLocalPoint);
+            Gizmos.DrawWireSphere(refWorlPoint, .15f);
+            Gizmos.DrawWireSphere(relWorldPoint, .15f);
+
+            Gizmos.color = origColor;
+
+            // Change base to origin's local space so projecting to (0, 0, 0) is valid.
+            Vector3 localPoint = refPoint.PointToLocalSpace(relPoint);
+
+            // xy
+            Vector3 localNormal = refPoint.DirectionToLocalSpace(refPoint.forward).normalized;
+            Vector3 projPoint = localPoint.Project(localNormal);
+            Gizmos.color = zAxisColour;
+            Gizmos.DrawLine(Vector3.zero, projPoint);
+
+            // zx
+            localNormal = refPoint.DirectionToLocalSpace(refPoint.up).normalized;
+            projPoint = localPoint.Project(localNormal);
+            Gizmos.color = yAxisColour;
+            Gizmos.DrawLine(Vector3.zero, projPoint);
+
+            // zy
+            localNormal = refPoint.DirectionToLocalSpace(-refPoint.right).normalized;
+            projPoint = localPoint.Project(localNormal);
+            Gizmos.color = xAxisColour;
+            Gizmos.DrawLine(Vector3.zero, projPoint);
+
+            Gizmos.color = localSpaceColour;
+        }
+
+        void OnGUI() {
+            GUI.Label(new Rect(50, 50, 500, 20), string.Format("Vector angle from reference to point: {0:0.0000}", refPoint.forward.AngleTo(relPoint - refPoint.position)));
+            GUI.Label(new Rect(50, 70, 500, 20), string.Format("Transform angle from reference to point: {0:0.0000}", refPoint.AngleTo(relPoint)));
+
+            GUI.Label(new Rect(50, 90, 500, 20), string.Format("XZ plane angle from reference to point: {0:0.0000}", refPoint.AngleZX(relPoint)));
+            GUI.Label(new Rect(50, 110, 500, 20), string.Format("XY plane angle from reference to point: {0:0.0000}", refPoint.AngleXY(relPoint)));
+            GUI.Label(new Rect(50, 130, 500, 20), string.Format("ZY plane angle from reference to point: {0:0.0000}", refPoint.AngleZY(relPoint)));
+
+            GUI.Label(new Rect(50, 170, 500, 20), string.Format("Origin.up: {0} Origin.forward: {1} Origing.right: {2}", refPoint.up, refPoint.forward, refPoint.right));
+        }
+
+        private void DrawAxisGizmos(Vector3 origin, Quaternion rotation) {
+            Gizmos.color = xAxisColour;
+            Gizmos.DrawLine(new Vector3(-10, 0, 0).TransformPoint(origin, rotation),
+                            new Vector3(10, 0, 0).TransformPoint(origin, rotation));
+            for (int i = -10; i < 11; ++i) {
+                Gizmos.DrawLine(new Vector3(i, 0, -.1f).TransformPoint(origin, rotation),
+                                new Vector3(i, 0, .1f).TransformPoint(origin, rotation));
+            }
+
+            Gizmos.color = yAxisColour;
+            Gizmos.DrawLine(new Vector3(0, -10, 0).TransformPoint(origin, rotation),
+                            new Vector3(0, 10, 0).TransformPoint(origin, rotation));
+            for (int i = -10; i < 11; ++i) {
+                Gizmos.DrawLine(new Vector3(0, i, -.1f).TransformPoint(origin, rotation),
+                                new Vector3(0, i, .1f).TransformPoint(origin, rotation));
+            }
+
+            Gizmos.color = zAxisColour;
+            Gizmos.DrawLine(new Vector3(0, 0, -10).TransformPoint(origin, rotation),
+                            new Vector3(0, 0, 10).TransformPoint(origin, rotation));
+            for (int i = -10; i < 11; ++i) {
+                Gizmos.DrawLine(new Vector3(-.1f, 0, i).TransformPoint(origin, rotation),
+                                new Vector3(.1f, 0, i).TransformPoint(origin, rotation));
+            }
+        }
+    }
+}
--- Test/Scripts/TestVector3Extensions.cs
+++ Test/Scripts/TestVector3Extensions.cs
@@ -8,26 +8,51 @@ public class TestVector3Extensions : MonoBehaviour {
         rotation = Quaternion.Euler( new Vector3( 0, 0, Time.time * 10 ) );
     }
 
+    void OnGUI() {
+        foreach (var cam in GameObject.FindObjectsOfType<Camera>()) {
+            GUI.Label( new Rect(50, 50, Screen.width, Screen.height ),
+                       "Check Scene tab to see if projections and transformations work properly." );
+        }
+    }
+
     private Vector3 point = new Vector3( 3, 2, 4 );
     Quaternion rotation = Quaternion.Euler( 0, 0, 0 );
     void OnDrawGizmos() {
         Color prevColor = Gizmos.color;
         Gizmos.color = Color.white;
 
+        Vector3 secondPoint = (point * 2).RotateAround(point, rotation);
+
         Gizmos.DrawLine( Vector3.zero, point.Rotate( rotation ) );
-        Gizmos.DrawLine( point, (point * 2).RotateAround( point, rotation ) );
+        Gizmos.DrawLine( point, secondPoint );
 
         foreach (var cam in GameObject.FindObjectsOfType<Camera>()) {
             Gizmos.color = Color.green;
             Gizmos.DrawLine( Vector3.zero.Project( cam.transform.forward ), point.Rotate( rotation ).Project( cam.transform.forward ) );
-            Gizmos.DrawLine( point.Project( cam.transform.forward ), ((point * 2).RotateAround( point, rotation )).Project( cam.transform.forward ) );
+            Gizmos.DrawLine( point.Project( cam.transform.forward ), secondPoint.Project( cam.transform.forward ) );
 
             Gizmos.color = Color.blue;
             Gizmos.DrawLine( Vector3.zero.Project( cam.transform.position, cam.transform.rotation ),
                              point.Rotate( rotation ).Project( cam.transform.position, cam.transform.rotation ) );
             Gizmos.DrawLine( point.Project( cam.transform.position, cam.transform.rotation ),
-                             ((point * 2).RotateAround( point, rotation )).Project( cam.transform.position, cam.transform.rotation ) );
+                             (secondPoint).Project( cam.transform.position, cam.transform.rotation ) );
         }
+
+        // These two points should never move from (1, 2, 3)
+        Vector3 worldPoint = new Vector3( 1, 2, 3 );
+        Vector3 localPoint = worldPoint.InverseTransformPoint(secondPoint, rotation);
+        Vector3 newWorldPoint = localPoint.TransformPoint(secondPoint, rotation);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(worldPoint, .25f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(newWorldPoint, .5f);
+
+        // This should move with the target point, always at the same distance, rotating around it.
+        Vector3 constantLocalPoint = new Vector3(1, 2, 3);
+        Vector3 constantWorldPoint = constantLocalPoint.TransformPoint(secondPoint, rotation);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(constantWorldPoint, .75f);
 
         Gizmos.color = prevColor;
     }
